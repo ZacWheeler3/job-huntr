9d87561b8d75dd3e3ea38fdb3a9af347f44d912e
Zac Wheeler <wheelerzac@yahoo.com>
2023-08-22 22:23:24 -0500

    added icons to login

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import ThoughtList from "../components/ThoughtList";
 import ThoughtForm from "../components/ThoughtForm";
 
 import { QUERY_THOUGHTS } from "../utils/queries";
+import { Iceburger } from "react-iceburger";
 
 const Home = () => {
   return (
@@ -12,12 +13,13 @@ const Home = () => {
       <p className="links">
         <a href="">User Settings</a>
         <a href="">My Terms</a>
-        <a href=""><LINK:atom></LINK:atom></a>
+        <a href=""></a>
 
         <a href="">My Applications</a>
         <a href="">Follow-Ups</a>
         <a href="">Common Questions</a>
       </p>
+      <Iceburger color="white" />
       <div className="flex-row justify-center">
         <div
           className="col-12 col-md-10 mb-3 p-3"
