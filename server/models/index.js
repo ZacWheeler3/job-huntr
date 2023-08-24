d5d78194136d4f1f3fc7f4daad9b77a50793f9bc
Hailey Hughes <haileyhughes@Haileys-MBP.attlocal.net>
2023-08-24 12:10:09 -0500

    updated index to include caq

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Job = require('./Job');
 const Contact = require('./Contact');
+const CommonQuestions = require('./CommonQuestions')
 
 
-module.exports = { User, Job, Contact };
+module.exports = { User, Job, Contact, CommonQuestions };
