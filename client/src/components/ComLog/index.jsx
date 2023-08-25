9ae10c6e10a9b816836780e6ce202b46f9d7d511
DaveFMiranda <127264662+DaveFMiranda@users.noreply.github.com>
2023-08-25 12:58:44 -0500

    Update ComLog component on front end

--- client/src/components/ComLog/index.jsx
+++ client/src/components/ComLog/index.jsx
@@ -1,20 +1,109 @@
-const ComLog = ({ comLogs = [] }) => {
-  if (!comLogs.length) {
-    return <h3>No Communication Yet</h3>;
+import {useState} from 'react';
+import {ADD_COMLOG} from '../../utils/mutations';
+import {useMutation} from "@apollo/client";
+
+const ComLog = ({ comLogs = [], jobId }) => {
+  const [method, setMethod] = useState("");
+  const [content, setContent] = useState("");
+  const [direction, setDirection] = useState("");
+
+  const [addComLog, { error }] = useMutation(ADD_COMLOG);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addComLog({
+        variables: {
+          jobId,
+          method,
+          content,
+          direction,
+        },
+      });
+      setMethod("");
+      setContent("");
+      setDirection("");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  
+  const addLogButtonHandler = () => {
+
   }
 
   return (
-    <ul>
-      {comLogs.map((comLog, index) => {
-        console.log(comLog);
-        return (
-          <li key={index}>
-            Method: {comLog.method}, Content: {comLog.content}, Direction:
-            {comLog.direction}
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <div className="form-group">
+        <button
+          className="btn btn-primary"
+          type="submit"
+          onClick={addLogButtonHandler}
+        >
+          Add Communication
+        </button>
+      </div>
+
+      <div>
+        <h3>Add a Communication</h3>
+
+        <form onSubmit={handleFormSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              name="method"
+              placeholder="Method"
+              value={method}
+              onChange={(e) => setMethod(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              name="content"
+              placeholder="Content"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              required
+            />
+          </div>{" "}
+          <div className="form-group">
+            <input
+              type="text"
+              name="direction"
+              placeholder="Direction"
+              value={direction}
+              onChange={(e) => setDirection(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-primary" type="submit">
+              Submit Communication
+            </button>
+          </div>
+        </form>
+      </div>
+
+
+      <ul>
+        {!comLogs.length ? (
+          <h3>No Communication Yet</h3>
+        ) : (
+          comLogs.map((comLog, index) => {
+            console.log(comLog);
+            return (
+              <li key={index}>
+                Method: {comLog.method}, Content: {comLog.content}, Direction:
+                {comLog.direction}
+              </li>
+            );
+          })
+        )}
+      </ul>
+    </>
   );
 };
 
