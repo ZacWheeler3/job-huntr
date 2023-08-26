e4c58aea0bac9d6e9b8482ed5b12be39a1760a32
Creature Feature <ivy.mccurdy@gmail.com>
2023-08-25 19:03:32 -0500

    tested login/signup functions. All looks good

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -6,7 +6,7 @@ const AboutPage = () => {
 return (
     <div className="about-container">
 <div className="about-title">
-    <h2>About the Creators</h2>
+    <h2>About the Creators!</h2>
 </div>
 <div className="creator-section">
 
