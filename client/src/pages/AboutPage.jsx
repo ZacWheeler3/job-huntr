bd44380b8e6a3130de48bcce3f3e456bf7fc0ad5
Zac Wheeler <wheelerzac@yahoo.com>
2023-08-25 23:07:33 -0500

    added links to each person on about

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,23 +1,175 @@
 import React from "react";
-import { FaGithub } from "react-icons/fa";
-
+import {
+  FaGithub as GithubIcon,
+  FaLinkedin as LinkedInIcon,
+} from "react-icons/fa";
+import zac from "../../public/zac.jpg?url";
 
 const AboutPage = () => {
-return (
+  return (
     <div className="about-container">
-<div className="about-title">
-    <h2>About the Creators!</h2>
-</div>
-<div className="creator-section">
+      <div className="about-title">
+        <h2>Meet Our Team!</h2>
+      </div>
 
-
-
-</div>
+      <div className="creator-section">
+        <div className="dev-name">Hailey Hughes</div>
+        <div className="dev-pic">
+          <img src={zac} alt="My Image" />
+        </div>
+        <div className="dev-creds">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+            odio, obcaecati molestiae consequuntur corporis earum voluptatem
+            labore, perspiciatis qui illo rem ex nemo optio! Corrupti voluptatem
+            quod eligendi et repellat! Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Eligendi iusto aut quidem et dolorum labore nisi
+            atque, sit tempore quam cupiditate obcaecati doloremque odit quis
+            harum? Odio eligendi rerum quis?Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Reiciendis numquam distinctio optio
+            suscipit, sint saepe dolorum. Cupiditate, asperiores laudantium quo
+            voluptatum odio harum, veniam ullam suscipit odit placeat laborum
+            molestias!
+          </p>
+          <ul>
+            <li>
+              <a href="https://github.com/haileyraehughes89">
+                <GithubIcon />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/hailey-hughes-039382206">
+                <LinkedInIcon />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="dev-name">Dave Miranda</div>
+        <div className="dev-pic">
+          <img src={zac} alt="My Image" />
+        </div>
+        <div className="dev-creds">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+            odio, obcaecati molestiae consequuntur corporis earum voluptatem
+            labore, perspiciatis qui illo rem ex nemo optio! Corrupti voluptatem
+            quod eligendi et repellat! Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Eligendi iusto aut quidem et dolorum labore nisi
+            atque, sit tempore quam cupiditate obcaecati doloremque odit quis
+            harum? Odio eligendi rerum quis?Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Reiciendis numquam distinctio optio
+            suscipit, sint saepe dolorum. Cupiditate, asperiores laudantium quo
+            voluptatum odio harum, veniam ullam suscipit odit placeat laborum
+            molestias!
+          </p>
+          <ul>
+            <li>
+              <a href="https://github.com/DaveFMiranda">
+                <GithubIcon />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/dave-m-9463327">
+                <LinkedInIcon />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="dev-name">Ivy McCurdy</div>
+        <div className="dev-pic">
+          <img src={zac} alt="My Image" />
+        </div>
+        <div className="dev-creds">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+            odio, obcaecati molestiae consequuntur corporis earum voluptatem
+            labore, perspiciatis qui illo rem ex nemo optio! Corrupti voluptatem
+            quod eligendi et repellat! Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Eligendi iusto aut quidem et dolorum labore nisi
+            atque, sit tempore quam cupiditate obcaecati doloremque odit quis
+            harum? Odio eligendi rerum quis?Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Reiciendis numquam distinctio optio
+            suscipit, sint saepe dolorum. Cupiditate, asperiores laudantium quo
+            voluptatum odio harum, veniam ullam suscipit odit placeat laborum
+            molestias!
+          </p>
+          <ul>
+            <li>
+              <a href="https://github.com/IvySMac">
+                <GithubIcon />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/ivy-mccurdy">
+                <LinkedInIcon />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="dev-name">Christopher Tsang</div>
+        <div className="dev-pic">
+          <img src={zac} alt="My Image" />
+        </div>
+        <div className="dev-creds">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+            odio, obcaecati molestiae consequuntur corporis earum voluptatem
+            labore, perspiciatis qui illo rem ex nemo optio! Corrupti voluptatem
+            quod eligendi et repellat! Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Eligendi iusto aut quidem et dolorum labore nisi
+            atque, sit tempore quam cupiditate obcaecati doloremque odit quis
+            harum? Odio eligendi rerum quis?Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Reiciendis numquam distinctio optio
+            suscipit, sint saepe dolorum. Cupiditate, asperiores laudantium quo
+            voluptatum odio harum, veniam ullam suscipit odit placeat laborum
+            molestias!
+          </p>
+          <ul>
+            <li>
+              <a href="https://github.com/wontonchris">
+                <GithubIcon />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/christopher-tsang-97710386">
+                <LinkedInIcon />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="dev-name">Zac Wheeler</div>
+        <div className="dev-pic">
+          <img src={zac} alt="My Image" />
+        </div>
+        <div className="dev-creds">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+            odio, obcaecati molestiae consequuntur corporis earum voluptatem
+            labore, perspiciatis qui illo rem ex nemo optio! Corrupti voluptatem
+            quod eligendi et repellat! Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Eligendi iusto aut quidem et dolorum labore nisi
+            atque, sit tempore quam cupiditate obcaecati doloremque odit quis
+            harum? Odio eligendi rerum quis?Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Reiciendis numquam distinctio optio
+            suscipit, sint saepe dolorum. Cupiditate, asperiores laudantium quo
+            voluptatum odio harum, veniam ullam suscipit odit placeat laborum
+            molestias!
+          </p>
+          <ul>
+            <li>
+              <a href="https://github.com/ZacWheeler3">
+                <GithubIcon />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/zac-wheeler-76335b180">
+                <LinkedInIcon />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-)
-
-
-
-
-}
+  );
+};
 export default AboutPage;
