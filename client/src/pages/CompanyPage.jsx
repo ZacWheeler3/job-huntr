aec0911df7cb8f41612172c44686212dc83e94ec
Zac Wheeler <wheelerzac@yahoo.com>
2023-08-25 21:10:58 -0500

    added company page content

--- client/src/pages/CompanyPage.jsx
+++ client/src/pages/CompanyPage.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 import { FaGithub } from "react-icons/fa";
-
-
 const CompanyPage = () => {
-return (
-    <div className="about-container">
-<div className="about-title">
-    <h2>Jobhuntr Mission </h2>
-    <p>lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque repudiandae perferendis laboriosam maiores tempora. Minima asperiores architecto illo nesciunt corporis, perspiciatis quam officiis! Iusto odit aliquam beatae eveniet eum necessitatibus.   </p>
-</div>
-<div className="creator-section">
-
-
-
-</div>
+  return (
+    <div className="company-container">
+      <div className="company-title">
+        <h2> Our Mission </h2>
+        <h3>Empowering Your Journey: Our Mission to Get <span>&nbsp;You&nbsp;</span> Hired!</h3>
+        <p>
+        At <span>Job Huntr</span>, our unwavering mission is to empower individuals on their journey to meaningful employment and career advancement. We recognize that the path to finding the right job can be both exhilarating and challenging, and we are dedicated to being your steadfast companion throughout this transformative process.{" "}
+        </p>
+        <p>
+        Our platform is built on the belief that every individual possesses unique talents, skills, and aspirations that deserve to be matched with the perfect career opportunity. We are committed to facilitating connections that go beyond mere job listings. We aim to foster relationships between candidates and employers, ensuring that the right talents find their rightful place within companies that appreciate and nurture their potential.{" "}
+        </p>
+        <p>At <span>Job Huntr</span>, we are dedicated to being more than just a job board. We are your partner, your advocate, and your guide as you take the next step in your professional journey. Join us in shaping a future where individuals are not just hired, but truly thrive in careers that fulfill their potential and aspirations.</p>
+      </div>
+      <div className="creator-section"></div>
     </div>
-)
-
-
-
-
-}
+  );
+};
 export default CompanyPage;
