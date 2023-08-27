9af53da667ee17e1173f0771400c34c9d3490dc7
Hailey Hughes <haileyhughes@Haileys-MBP.attlocal.net>
2023-08-27 13:03:25 -0500

    caq form to create new questions. route works.

---
+++ client/src/components/QuestionForm/index.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+
+// import { ADD_THOUGHT } from '../../utils/mutations';
+import { ADD_QUESTION } from "../utils/mutations";
+
+import Auth from "../utils/auth";
+
+const CommonQuestions = () => {
+  const [question, setQuestion] = useState("");
+  const [response, setResponse] = useState("");
+
+
+  const [addQuestion, { error }] = useMutation(ADD_QUESTION);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+
+    try {
+      const { data } = await addQuestion({
+        variables: {
+          question,
+          response,
+        },
+      });
+      setQuestion("");
+      setResponse("");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <h3>Add a Question</h3>
+      {Auth.loggedIn() ? (
+        <>
+          <form onSubmit={handleFormSubmit}>
+            <div className="form-group">
+              <input
+                type="text"
+                name="question"
+                placeholder="New Question Here"
+                value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <input
+                type="text"
+                name="response"
+                placeholder="Response Here"
+                value={response}
+                onChange={(e) => setResponse(e.target.value)}
+                required
+              />
+            </div>
+
+          
+
+
+     
+
+
+            <div className="form-group">
+              <button className="btn btn-primary" type="submit">
+                Add Question
+              </button>
+            </div>
+
+            {error && (
+              <div className="bg-danger text-white p-3">{error.message}</div>
+            )}
+          </form>
+        </>
+      ) : (
+        <p>
+          You need to be logged in to add a job. Please{" "}
+          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default CommonQuestions;
