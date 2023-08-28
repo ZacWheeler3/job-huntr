6c8cb6d4ee70ad352ea197191832b02b79fb57e2
Zac Wheeler <wheelerzac@yahoo.com>
2023-08-27 20:34:50 -0500

    added zac's about me

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -51,9 +51,26 @@ const AboutPage = () => {
         </div>
         <div className="dev-creds">
           <p>
-          Dave Miranda is a devoted father to his two sons, 5 and 3 years old. Their favorite things to do together include Legos, building forts, indoor playgrounds, and children's museums. When he's not playing with his kids, Dave enjoys learning new things, self-improvement, winning his fantasy football league more often than not, and Wordle.</p>
-<p>A native Chicagoan and avid football watcher, Dave should be a Bears fan but they're just such a poorly-run franchise year in and year out, so he also roots for the Chiefs for two reasons: #1 He moved to the KC area in 2016, and #2 He cruised to a dominant fantasy football championship in 2018 after drafting the previously unheralded duo of Patrick Mahomes and Tyreek Hill.</p>
-<p>Prior to learning to code, Dave ran political campaigns for about a decade. In 2018 he started a consulting firm and managed six candidates and a staff of several dozen campaign workers. He has run campaigns with budgets ranging from $30,000 to $500,000. Before his political career, Dave worked in corporate public relations.
+            Dave Miranda is a devoted father to his two sons, 5 and 3 years old.
+            Their favorite things to do together include Legos, building forts,
+            indoor playgrounds, and children's museums. When he's not playing
+            with his kids, Dave enjoys learning new things, self-improvement,
+            winning his fantasy football league more often than not, and Wordle.
+          </p>
+          <p>
+            A native Chicagoan and avid football watcher, Dave should be a Bears
+            fan but they're just such a poorly-run franchise year in and year
+            out, so he also roots for the Chiefs for two reasons: #1 He moved to
+            the KC area in 2016, and #2 He cruised to a dominant fantasy
+            football championship in 2018 after drafting the previously
+            unheralded duo of Patrick Mahomes and Tyreek Hill.
+          </p>
+          <p>
+            Prior to learning to code, Dave ran political campaigns for about a
+            decade. In 2018 he started a consulting firm and managed six
+            candidates and a staff of several dozen campaign workers. He has run
+            campaigns with budgets ranging from $30,000 to $500,000. Before his
+            political career, Dave worked in corporate public relations.
           </p>
           <ul>
             <li>
@@ -136,17 +153,29 @@ const AboutPage = () => {
         </div>
         <div className="dev-creds">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
-            odio, obcaecati molestiae consequuntur corporis earum voluptatem
-            labore, perspiciatis qui illo rem ex nemo optio! Corrupti voluptatem
-            quod eligendi et repellat! Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Eligendi iusto aut quidem et dolorum labore nisi
-            atque, sit tempore quam cupiditate obcaecati doloremque odit quis
-            harum? Odio eligendi rerum quis?Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Reiciendis numquam distinctio optio
-            suscipit, sint saepe dolorum. Cupiditate, asperiores laudantium quo
-            voluptatum odio harum, veniam ullam suscipit odit placeat laborum
-            molestias!
+            Hello! I'm Zac Wheeler, a devoted husband and a proud father of two
+            amazing kids—a lively 6-year-old and an adorable 2-year-old. As a
+            family man, I find immense joy in spending quality time with my
+            loved ones. Whether we're embarking on outdoor adventures, engaging
+            in playful board games, or simply enjoying each other's presence at
+            home, family always comes first.
+          </p>
+
+          <p>
+            When I'm not wearing my dad hat, you'll likely find me indulging in
+            two of my passions: movies and 3D printing. As a film enthusiast, I
+            relish movie nights with a big bowl of popcorn in hand. From
+            timeless classics to the latest blockbusters, count me in! On the
+            creative front, I'm also an enthusiastic 3D printing aficionado.
+            There's something uniquely satisfying about transforming digital
+            designs into tangible objects, and I'm constantly tinkering with my
+            printer, turning ideas into reality.
+          </p>
+
+          <p>
+            Life is a delicate balance, and for me, that entails treasuring
+            family moments, immersing myself in cinematic wonders, and diving
+            into the world of 3D printing!
           </p>
           <ul>
             <li>
