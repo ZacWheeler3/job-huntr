931f3e9ecce5a8ada1b57e0431f2233bb1f585af
Noob Evad <davefrankmiranda@gmail.com>
2023-08-28 10:16:25 -0500

    removed old source code

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -46,23 +46,6 @@ export const QUERY_QUESTION = gql`
   }
 `;
 
-export const QUERY_SINGLE_THOUGHT = gql`
-  query getSingleThought($thoughtId: ID!) {
-    thought(thoughtId: $thoughtId) {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-      comments {
-        _id
-        commentText
-        commentAuthor
-        createdAt
-      }
-    }
-  }
-`;
-
 export const QUERY_ME = gql`
   query me {
     me {
